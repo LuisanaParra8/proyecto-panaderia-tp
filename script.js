28b76aaa69695e30d055e3e35dc5fad194b69f32
juli <julianaluz.ledesma@icloud.com>
2025-10-26 18:52:24 -0300

    Agregar evento click al botón de panadería

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ window.onload = function() {
     alert("¡Bienvenido/a a Le Croissant! 🥐 Esperamos que disfrutes nuestros productos.");
     const btnPanaderia = document.getElementById("btn-panaderia");
     if (btnPanaderia) {
+        btnPanaderia.addEventListener("click", function() {});
         }
 };
