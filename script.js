7bdf863628c29b0c47d288402a572b4ec8ff15d3
juli <julianaluz.ledesma@icloud.com>
2025-10-26 18:51:03 -0300

    Validar existencia del botón de panadería

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.onload = function() {
     alert("¡Bienvenido/a a Le Croissant! 🥐 Esperamos que disfrutes nuestros productos.");
     const btnPanaderia = document.getElementById("btn-panaderia");
+    if (btnPanaderia) {
+        }
 };
