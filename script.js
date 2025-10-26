f1f3090400b1b27b3700027aad61c8cbcc18ad55
juli <julianaluz.ledesma@icloud.com>
2025-10-26 18:57:28 -0300

    Seleccionar productos extra de panadería

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ window.onload = function() {
     alert("¡Bienvenido/a a Le Croissant! 🥐 Esperamos que disfrutes nuestros productos.");
     const btnPanaderia = document.getElementById("btn-panaderia");
     if (btnPanaderia) {
-        btnPanaderia.addEventListener("click", function() {});
-        }
+    btnPanaderia.addEventListener("click", function(){
+     const extraPanaderia = document.querySelector(".extra-productos");
+   
+    });
+        
+  }
 };
