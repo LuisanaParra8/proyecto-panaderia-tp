5bfbf64d2b1a58270ea8499fdca1e4341c56b9c3
juli <julianaluz.ledesma@icloud.com>
2025-10-26 18:49:10 -0300

    Definir botón de panadería

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 window.onload = function() {
     alert("¡Bienvenido/a a Le Croissant! 🥐 Esperamos que disfrutes nuestros productos.");
+    const btnPanaderia = document.getElementById("btn-panaderia");
 };
