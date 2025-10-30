846ff7d890fb74a04b6aa46b506d97374982d33a
valentina <valentinapelliza1@gmail.com>
2025-10-29 21:38:03 -0300

    Corrigiendo errores

---
+++ widi/ejemplo.js
@@ -0,0 +1,7 @@
+console.log("Romi se porta bien");
+console.warn("La valen es mas o menos inteligente");
+console.error("error en el sistema");
+console.debug("document");
+console.info("doccument");
+console.dir("document");
+console.trace("document");
