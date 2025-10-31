9896f4667abd4b427e4246f2c9018e95033b6ca4
LuisanaParra8 <luisanajparra@gmail.com>
2025-10-31 14:06:04 -0300

    Configurando función del button

--- script1.js
+++ script1.js
@@ -1,1 +1,9 @@
 const btnArriba = document.getElementById("btnArriba");
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY>200){
+    btnArriba.style.display = "block";
+  }else{
+    btnArriba.style.display = "none";
+  }
+});
