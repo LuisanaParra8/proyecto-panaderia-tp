f8a0b3b7ae8eec6bcfee65494f1dcbfbda9bafdb
lishlt <lisholt86@gmail.com>
2025-11-04 09:01:55 -0300

    Agregando efecto a redes sociales

--- script2.js
+++ script2.js
@@ -13,4 +13,19 @@ btnArriba.addEventListener("click", () => {
     top: 0,
     behavior: "smooth"
   });
+  const facebook = document.getElementById("facebook");
+  facebook.addEventListener("mouseenter", () => {
+    facebook.style.color = "#1877f2"; 
+  });
+  facebook.addEventListener("mouseleave", () => {
+    facebook.style.color = "#000"; 
+  });
+  
+  const instagram = document.getElementById("instagram");
+  instagram.addEventListener("mouseenter", () => {
+    instagram.style.color = "#e1306c"; 
+  });
+  instagram.addEventListener("mouseleave", () => {
+    instagram.style.color = "#000";
+  });
 });
