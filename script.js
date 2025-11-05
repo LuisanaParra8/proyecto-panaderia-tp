58b5032906aa3e1afd5970338bdb098f4807f7b9
rominasal12 <rominasalvay1@gmail.com>
2025-11-04 21:15:13 -0300

    Agregando un if else de boton

--- script.js
+++ script.js
@@ -31,3 +31,10 @@ window.onload = function() {
   }
 };
 const btnArriba = document.getElementById("btnArriba");
+window.addEventListener("scroll", () => {
+  if(window.scrollY>200){
+    btnArriba.style.display = "block";
+  }else{
+    btnArriba.style.display = "none";
+  }
+});
